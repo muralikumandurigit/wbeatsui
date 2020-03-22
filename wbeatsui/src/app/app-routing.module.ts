a356a01130be71c5b1d055830e6d9a303fabd0c5
ADMIN <muralikumanduri@gmail.com>
2020-03-23 01:31:45 +0530

    Created router

--- wbeatsui/src/app/app-routing.module.ts
+++ wbeatsui/src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { StudentslistComponent } from './studentslist/studentslist.component';
+import { AppComponent } from './app.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: AppComponent },
+  { path: 'studentslist', component: StudentslistComponent },
+  { path: '**', component: AppComponent } // If no matching route found, go back to home route
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- wbeatsui/src/app/app.module.ts
+++ wbeatsui/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StudentslistComponent } from './studentslist/studentslist.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StudentslistComponent
   ],
   imports: [
     BrowserModule,
