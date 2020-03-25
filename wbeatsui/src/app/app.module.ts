b5b8d75725f00ff2e324ffd10dbb352625845c81
ADMIN <muralikumanduri@gmail.com>
2020-03-25 18:58:01 +0530

    banner created

--- wbeatsui/src/app/app.module.ts
+++ wbeatsui/src/app/app.module.ts
@@ -1,18 +1,41 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StudentslistComponent } from './studentslist/studentslist.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatCardModule } from '@angular/material/card';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { BannerComponent } from './banner/banner.component';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    StudentslistComponent
+    StudentslistComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatCardModule,
+    MatProgressSpinnerModule,
+    MatTableModule,
+    MatPaginatorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- wbeatsui/src/app/studentslist/studentslist.component.ts
+++ wbeatsui/src/app/studentslist/studentslist.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from '../data.service';
 
 @Component({
   selector: 'app-studentslist',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StudentslistComponent implements OnInit {
 
-  constructor() { }
+  students = [];
+
+  displayedColumns: string[] = ['student_id', 'first_name', 'middle_name', 'last_name',
+                                'dob', 'father_name', 'mother_name', 'guardian_name', 
+                                'start_date', 'end_date', 'status_code', 'comments'];
+
+  constructor(private dataService : DataService) { }
 
   ngOnInit(): void {
+    this.dataService.sendGetRequest().subscribe((data : any[]) => {
+      console.log(data);
+      this.students = data;
+    });
   }
 
 }
