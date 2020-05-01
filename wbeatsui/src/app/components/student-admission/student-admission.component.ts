70d7271cbd7a4049a71a8071101bf6324084f87d
ADMIN <muralikumanduri@gmail.com>
2020-05-02 03:25:22 +0530

    Started implementing new student admission

--- wbeatsui/src/app/components/student-admission/student-admission.component.ts
+++ wbeatsui/src/app/components/student-admission/student-admission.component.ts
@@ -1,12 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import {FormControl} from '@angular/forms';
 
+export interface Gender {
+  code: string,
+  description: string
+}
+
 @Component({
   selector: 'app-student-admission',
   templateUrl: './student-admission.component.html',
   styleUrls: ['./student-admission.component.css']
 })
 export class StudentAdmissionComponent implements OnInit {
+  
+  gender : Gender[] = [
+      {code: 'M', description: 'Male'},
+      {code: 'F', description: 'Female'}
+  ];
   
   today = new FormControl(new Date());
   constructor() { }
