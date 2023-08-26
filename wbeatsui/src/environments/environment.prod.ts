0a49a7317bbc3dafed3e29b0137235bdc3fe60c7
Kumanduri Murali Krishna <muralikumanduri@gmail.com>
2023-08-26 08:34:01 +0530

    adding env files

---
+++ wbeatsui/src/environments/environment.prod.ts
@@ -0,0 +1,9 @@
+// environment.ts
+export const environment = {
+    production: true,
+    msg: "I am from prod",
+    client_id: "519057022096-ag98hjt4l1snotolc1vaa303c7tpqgsg.apps.googleusercontent.com",
+    client_secret: "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"
+    // Other environment-specific variables
+  };
+  
---
+++ wbeatsui/src/environments/environment.ts
@@ -0,0 +1,7 @@
+// environment.ts
+export const environment = {
+    production: false,
+    msg: "I am from dev"
+    // Other environment-specific variables
+  };
+  
